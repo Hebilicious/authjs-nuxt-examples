dd9fb1041a0ec8d4cbfd895b6258ed651d04f031
Hebilicious <xsh4k3@gmail.com>
2023-10-18 17:37:08 +0700

    chore: release v0.3.5

---
+++ server/api/auth/[...].ts
@@ -0,0 +1,20 @@
+import GithubProvider from "@auth/core/providers/github"
+import type { AuthConfig } from "@auth/core/types"
+import { NuxtAuthHandler } from "#auth"
+
+const runtimeConfig = useRuntimeConfig()
+
+export const authOptions: AuthConfig = {
+  secret: runtimeConfig.authJs.secret,
+  theme: {
+    logo: "https://nuxt.com/assets/design-kit/logo/icon-green.png"
+  },
+  providers: [
+    GithubProvider({
+      clientId: runtimeConfig.github.clientId,
+      clientSecret: runtimeConfig.github.clientSecret
+    })
+  ]
+}
+
+export default NuxtAuthHandler(authOptions, runtimeConfig)
